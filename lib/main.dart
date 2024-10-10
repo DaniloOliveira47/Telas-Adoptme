0263bcf1b585fc60addff90c215aae5e8609ccb5
danilo <danilo.7.oliveira2006@gmail.com>
2024-10-10 15:48:45 -0300

    Primeira postagem

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:project/view/cadastro.dart';
+import 'package:project/view/login.dart';
+import 'package:project/view/navigation.dart';
+import 'package:project/view/usuario.dart';
+
+void main() {
+  runApp(const MyApp());
+  
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BottomNavigationBarExample(),
+    );
+  }
+}
+
